377029e7a8b672a7035fdd7f60bd1da9c2095d0c
Crpearce <Crpearce892@gmail.com>
2023-05-09 10:30:33 -0600

    remove a briggs photo

--- src/routes/gallery/gallery.component.jsx
+++ src/routes/gallery/gallery.component.jsx
@@ -4,37 +4,11 @@ import PhotosSection from "../../components/photosSection/photosSection.componen
 import "./gallery.styles.css";
 
 const Gallery = ({ projectPhotos }) => {
-  // const [activeJob, setActiveJob] = useState("Boulder Remodel");
-
-  // const handleClick = (event) => setActiveJob(event.target.className);
 
   return (
     <section>
       <Banner sectionName="Gallery" />
       <div className="gallery-wrapper">
-        {/* <div className="gallery-nav">
-          {menu.map((project) => (
-            <span
-              className={project.location}
-              onClick={handleClick}
-              key={project.id}
-              id="gallery-nav-item"
-            >
-              {project.location}
-            </span>
-          ))}
-        </div> */}
-        {/* {activeJob === "Boulder Remodel" && (
-          <PhotosSection photos={boulderPhotos} />
-        )}
-        {activeJob === "Briggs Beam Installation" && (
-          <PhotosSection photos={briggsPhotos} />
-        )}
-
-        {activeJob === "Jensen Patio Renovation" && (
-          <PhotosSection photos={jensenPhotos} />
-        )} */}
-
         <PhotosSection photos={projectPhotos} />
       </div>
     </section>
